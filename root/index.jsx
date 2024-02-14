71cbb18f6eefcbb45da7c7aaab9e130d0b3fcb2a
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-14 17:14:30 -0300

    refactor folder delete

--- root/index.jsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app/app'
-import style from './../src/commom.css'
-
-ReactDOM.render(<App/>, document.getElementById('app'))
